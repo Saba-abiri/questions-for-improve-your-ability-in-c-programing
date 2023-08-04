8151786c07fa9b7fecf5679cd672ecf3fe38c8f0
saba <80123606+saba0909@users.noreply.github.com>
2023-08-04 07:55:43 +0330

    Add files via upload

---
+++ String_HMW/QString_ (8)/QString_8/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#define  SIZE 10
+void sortarry(char *str);
+void swpstr(char *str1,char *str2);
+int main()
+{
+    const char strary[][SIZE]={"ali","amir","sina","saba","akram"};
+    swpstr(strary);
+    puts(strary);
+}
+void sortarry(char *str)
+{
+    for(uint32_t i=0;i<)
+}
+void swpstr(char *str1,char *str2)
